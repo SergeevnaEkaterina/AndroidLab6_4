28dba16fcb2796226ce43dae35e310f113feab23
Ekaterina Gerasina <gerasinaes@mail.ru>
2021-11-26 14:27:01 +0300

    unused import removed

--- app/src/main/java/com/example/task4/MainActivity.kt
+++ app/src/main/java/com/example/task4/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import com.bumptech.glide.Glide
 import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
